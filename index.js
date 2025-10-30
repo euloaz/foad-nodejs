f4c7280916f5b3ca3a1422e717c390c59af0ab37
Héloïse <heloise.marie-oem@outlook.fr>
2025-10-30 11:27:59 +0100

    ajout compose + npm i faker

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const portServer = 8002;
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://host.docker.internal:27017/notes")
-  .then(() => console.log("Connecté à mongodb"))
-  .catch((err) => console.error("Erreur de connextion à mongodb !", err));
+    .connect("mongodb://host.docker.internal:27017/users")
+    .then(() => console.log("Connecté à mongodb"))
+    .catch((err) => console.error("Erreur de connextion à mongodb !", err));
 
-app.use("/api/users", noteRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(portServer, "0.0.0.0", () => {
-  console.log(`Le serveur tourne sur port ${portServer}`);
+    console.log(`Le serveur tourne sur port ${portServer}`);
 });
